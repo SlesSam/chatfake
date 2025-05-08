29971619c352672ac013e47f7d96682ecfac9955
SlesSam <lesy.sam9422@gmail.com>
2025-05-08 21:50:17 +0200

    redirct chat good

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,13 +1,37 @@
-import EmptyChat from "@/components/chats/chatEmpty";
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Chat } from '@/types/chat';
 
 export default function ChatPage() {
+  const router = useRouter();
 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    if (!user?.id) return;
 
-  return (
-    <main>
-      <EmptyChat />
-      hola welcom to chat
+    const stored = localStorage.getItem('chats');
+    const allChats = stored ? JSON.parse(stored) : [];
 
-    </main>
-  );
+   
+    const cleanedChats = allChats.filter((chat: Chat) =>
+      !(chat.userId === user.id && chat.title === 'Nuevo chat')
+    );
+
+ 
+    const newChat = {
+      id: Date.now().toString(),
+      userId: user.id,
+      title: 'Nuevo chat',
+      lastMessage: 'Empieza a chatear...',
+    };
+
+    const updatedChats = [newChat, ...cleanedChats];
+    localStorage.setItem('chats', JSON.stringify(updatedChats));
+
+    router.push(`/chats/${newChat.id}`);
+  }, []);
+
+  return null;
 }
