836a1ff0a2b2ccdbaeb121b00d34e0fa60f60eec
SlesSam <lesy.sam9422@gmail.com>
2025-05-09 18:24:10 +0200

    fixed tests settings

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+  },
+
   testMatch: ['**/__tests__/**/*.(spec|test).ts?(x)'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1' 
